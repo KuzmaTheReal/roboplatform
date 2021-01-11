1408f453418a28710b89c29aa6cbca6dbe1f0d02
AufstiegKrieg <69037148+AufstiegKrieg@users.noreply.github.com>
2021-01-11 15:11:59 +0300

    no debug

--- robotorium/settings.py
+++ robotorium/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+
+
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+#)
 
 AUTH_USER_MODEL = 'authapp.Teacher'
 
